cfd7f59afef97e66d3886e3bbbf86b41b345b15d
ADI NARAYANA REDDY <akumatha@gmail.com>
2023-05-31 11:59:47 +0530

    blinks

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
   
           <div className="card border-2">
               <div className="card-body">
-                <h3 className="card-header text-center"> Online Hotal Booking</h3>
+                <h3 className="card-header text-center  blink"> Online Hotal Booking</h3>
                 <p className="card-text">
                   
                   Allows hotel guests to schedule the dates of their stay,
@@ -42,7 +42,7 @@ const Projects = () => {
                   Number of Hires: <b>2</b>
                 </i>{" "}
                 <br />
-                <a href=""> Siva kala</a>
+                
                 <br />
               </div>
             </div>
@@ -52,7 +52,7 @@ const Projects = () => {
 
           <div className="card">
               <div className="card-body">
-                <h3 className="card-header text-center "> Online crime reporting system</h3>
+                <h3 className="card-header text-center  blink "> Online crime reporting system</h3>
                 <p className="card-text">
                   Online crime reporting system is a software that covers the entire matter control machine and this activity will help to handle all games in police station . This can take some getting used to Documenting of crimes and control of all games in police station through  computerized system.
                  </p>
@@ -84,7 +84,7 @@ const Projects = () => {
 
             <div className="card">
               <div className="card-body">
-                <h3 className="card-header text-center sh"> Fake Currency Detection</h3>
+                <h3 className="card-header text-center  blink sh"> Fake Currency Detection</h3>
                 <p className="card-text">
                   Fake Currency Detection is a task of binary classification in machine learning. If we have enough data on real and fake banknotes, we can use that data to train a model that can classify the new banknotes as real or fake.
                 </p>
@@ -109,7 +109,7 @@ const Projects = () => {
                   Number of Hires: <b>2</b>
                 </i>{" "}
                 <br />
-                <a href=""></a>
+                
                 <br />
               </div>
             </div>
@@ -122,7 +122,7 @@ const Projects = () => {
 
             <div className="card">
               <div className="card-body">
-                <h3 className="card-header text-center sh">employee payroll system</h3>
+                <h3 className="card-header text-center  blink sh">employee payroll system</h3>
                 <p className="card-text">
                   .      The payroll system is at the core of every organization’s human resource system. The remedy would take account of the estimation of the company’s payroll, the calculation of the income tax and the different deductions to be taken from the salary, including legislative deductions such as income tax and provident fund deductions.            </p>
                 <i>
@@ -146,14 +146,14 @@ const Projects = () => {
                   Number of Hires: <b>3</b>
                 </i>{" "}
                 <br />
-                <a href=""></a>
+                
                 <br />
               </div>
             </div>
 
             <div className="card">
               <div className="card-body">
-                <h3 className="card-header text-center sh">Student feedback system </h3>
+                <h3 className="card-header text-center  blink sh">Student feedback system </h3>
                 <p className="card-text">
                   Online Student Feedback System is the web based feedback collecting system from the students and provides the automatic generation of the feedback which is given by students. We have developed the student feedback system to provide feedback in an easy and quick manner to the college HOD. So we call it the Student Staff Feedback System.                  </p>
                 <i>
@@ -177,7 +177,7 @@ const Projects = () => {
                   Number of Hires: <b>3</b>
                 </i>{" "}
                 <br />
-                <a href=""></a>
+                
                 <br />
               </div>
             </div>
@@ -188,7 +188,7 @@ const Projects = () => {
 
             <div className="card">
               <div className="card-body">
-                <h3 className="card-header text-center sh">Student feedback system </h3>
+                <h3 className="card-header text-center  blink sh">Student feedback system </h3>
                 <p className="card-text">
                   Online Student Feedback System is the web based feedback collecting system from the students and provides the automatic generation of the feedback which is given by students. We have developed the student feedback system to provide feedback in an easy and quick manner to the college HOD. So we call it the Student Staff Feedback System.                  </p>
                 <i>
@@ -212,7 +212,7 @@ const Projects = () => {
                   Number of Hires: <b>3</b>
                 </i>{" "}
                 <br />
-                <a href=""></a>
+                
                 <br />
               </div>
             </div>
@@ -220,7 +220,7 @@ const Projects = () => {
 
             <div className="card">
               <div className="card-body">
-                <h3 className="card-header text-center sh">Develop online examination system </h3>
+                <h3 className="card-header text-center  blink sh">Develop online examination system </h3>
                 <p className="card-text">
                   The Online Examination System is developed using HTML and MySQL fully meets the objectives of the system for which it has been developed. ADMIN: The admin is responsible for Exam conduction. USER: The USER takes up the test by signing in and login in with his valid Login id and password.                  </p>
                 <i>
@@ -244,7 +244,7 @@ const Projects = () => {
                   Number of Hires: <b>3</b>
                 </i>{" "}
                 <br />
-                <a href=""></a>
+                
                 <br />
               </div>
             </div>
@@ -256,7 +256,7 @@ const Projects = () => {
 
 <div className="card">
               <div className="card-body">
-                <h3 className="card-header text-center sh">Student feedback system </h3>
+                <h3 className="card-header text-center  blink sh">Student feedback system </h3>
                 <p className="card-text">
                   Online Student Feedback System is the web based feedback collecting system from the students and provides the automatic generation of the feedback which is given by students. We have developed the student feedback system to provide feedback in an easy and quick manner to the college HOD. So we call it the Student Staff Feedback System.                  </p>
                 <i>
@@ -280,7 +280,7 @@ const Projects = () => {
                   Number of Hires: <b>3</b>
                 </i>{" "}
                 <br />
-                <a href=""></a>
+                
                 <br />
               </div>
             </div>
@@ -288,7 +288,7 @@ const Projects = () => {
 
             <div className="card">
               <div className="card-body">
-                <h3 className="card-header text-center sh"> Fire  Detection</h3>
+                <h3 className="card-header text-center  blink sh"> Fire  Detection</h3>
                 <p className="card-text">
                      Fire protection systems are standard building features that either actively or passively help to control the amount of damage that can occur to a building and protect its occupants in the event of a fire.       </p>
                 <i>
@@ -312,7 +312,7 @@ const Projects = () => {
                   Number of Hires: <b>2</b>
                 </i>{" "}
                 <br />
-                <a href=""></a>
+                
                 <br />
               </div>
             </div>
