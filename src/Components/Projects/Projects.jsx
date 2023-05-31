c26fe2b60d16541a8473f66e3de92bd12f67f660
ADI NARAYANA REDDY <akumatha@gmail.com>
2023-05-31 11:51:28 +0530

    chaige colors

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-
+import "./Projects.css"
 
 const Projects = () => {
 
@@ -52,7 +52,7 @@ const Projects = () => {
 
           <div className="card">
               <div className="card-body">
-                <h3 className="card-header text-center sh"><span>anil:-</span> Online crime reporting system</h3>
+                <h3 className="card-header text-center "> Online crime reporting system</h3>
                 <p className="card-text">
                   Online crime reporting system is a software that covers the entire matter control machine and this activity will help to handle all games in police station . This can take some getting used to Documenting of crimes and control of all games in police station through  computerized system.
                  </p>
